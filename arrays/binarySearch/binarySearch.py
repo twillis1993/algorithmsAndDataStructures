90063e26f3b0dacac3272f51addc1da8b0cf1067
Tom Willis <tom.willis@live.com>
2018-07-02 11:54:53 +0100

    Wrote binary search with index

--- arrays/binarySearch/binarySearch.py
+++ arrays/binarySearch/binarySearch.py
@@ -1,7 +1,3 @@
-import sys
-import argparse
-
-# TODO: unsorted input list?
 def binarySearch(inputList, value):
 	"""
 	Executes a binary search on the inputList.
@@ -19,9 +15,6 @@ def binarySearch(inputList, value):
 	-------
 	Bool.
 	"""
-	if type(value) != int:
-		raise TypeError('Parameter \'value\' must be an int.')
-
 	if not inputList:
 		return False
 	elif inputList[len(inputList)/2] == value:
@@ -31,3 +24,36 @@ def binarySearch(inputList, value):
 			return binarySearch(inputList[:len(inputList)/2],value)
 		else:
 			return binarySearch(inputList[(len(inputList)/2)+1:],value)
+
+def binarySearchWithIndex(inputList, value, index=0):
+	"""
+	Executes a binary search on the inputList.
+
+	Returns index if value present and -1 if not.
+
+	Parameters
+	----------
+	
+	inputList : list
+		List of ints to search
+
+	value : int
+		Value to find.
+
+	index : int
+		Index of first element of list in terms of original input list.
+	
+	Returns
+	-------
+	Int.
+	"""
+	if not inputList:
+		return -1
+	elif inputList[len(inputList)/2] == value:
+		return index+(len(inputList)/2)
+	else:	
+		if inputList[len(inputList)/2] > value:
+			return binarySearchWithIndex(inputList[:len(inputList)/2],value,index)
+		else:
+			return binarySearchWithIndex(inputList[(len(inputList)/2)+1:],value,index+(len(inputList)/2)+1)
+
--- arrays/binarySearch/test_binarySearch.py
+++ arrays/binarySearch/test_binarySearch.py
@@ -1,14 +1,8 @@
 import unittest
 import binarySearch
+import random
 
-class BinarySearchTest(unittest.TestCase):
-	
-	def test_errorFloatValue(self):
-		with self.assertRaises(TypeError) as e:
-			binarySearch.binarySearch([],1.0)
-
-		self.assertEqual('Parameter \'value\' must be an int.', str(e.exception))
-
+class BinarySearchTest(unittest.TestCase):	
 	def test_normalEmptyInputList(self):
 		self.assertEqual(False,binarySearch.binarySearch([],1))
 
@@ -29,3 +23,35 @@ class BinarySearchTest(unittest.TestCase):
 
 	def test_normalOddInputListPresent(self):
 		self.assertEqual(True,binarySearch.binarySearch([0,1,2,3,4],0))
+
+	def test_normalRandomEvenInputListPresent(self):	
+		for i in range(10):
+			self.assertEqual(True,binarySearch.binarySearch(list(range(1,101)),random.randint(1,100)))
+
+	def test_normalWithIndexEmptyList(self):
+		self.assertEqual(-1,binarySearch.binarySearchWithIndex([],1))
+
+	def test_normalWithIndexSingletonListValueAbsent(self):
+		self.assertEqual(-1,binarySearch.binarySearchWithIndex([0],1))
+
+	def test_normalWithIndexSingletonListValuePresent(self):
+		self.assertEqual(0,binarySearch.binarySearchWithIndex([1],1))
+
+	def test_normalEvenInputListAbsent(self):
+		self.assertEqual(-1,binarySearch.binarySearchWithIndex([0,1,2,3],4))
+			
+	def test_normalEvenInputListPresent(self):
+		self.assertEqual(0,binarySearch.binarySearchWithIndex([0,1,2,3],0))
+
+	def test_normalOddInputListAbsent(self):
+		self.assertEqual(-1,binarySearch.binarySearchWithIndex([0,1,2,3,4],5))
+
+	def test_normalOddInputListPresent(self):
+		self.assertEqual(0,binarySearch.binarySearchWithIndex([0,1,2,3,4],0))
+
+	def test_normalRandomEvenInputListPresent(self):	
+		for i in range(10):	
+			randValue = random.randint(1,100)
+			inputList = list(range(1,101))
+			self.assertEqual(inputList.index(randValue),
+			binarySearch.binarySearchWithIndex(inputList,randValue))
